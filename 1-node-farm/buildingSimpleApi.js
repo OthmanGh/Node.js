abe300e2ce39c978ccf7aa2b980f8dc9c248606b
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-01-07 21:22:36 +0300

    implement basic routing

--- 1-node-farm/buildingSimpleApi.js
+++ 1-node-farm/buildingSimpleApi.js
@@ -3,7 +3,20 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello From Server!!');
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('hello from Overview !!');
+  } else if (pathName === '/product') {
+    res.end('hello from Product !!');
+  } else if (pathName === '/api') {
+    res.end('hello from Api !!');
+  } else {
+    res.writeHead(404, {
+      'Content-text': 'text/html',
+    });
+    res.end(`<h1>404 error Page Not Found !!</h1>`);
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
