dee27f043596b4aa358bcbf49f2fa8e618834701
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-01-07 20:53:49 +0300

    creating server & start listenining to up coming requests

--- 1-node-farm/buildingSimpleApi.js
+++ 1-node-farm/buildingSimpleApi.js
@@ -1,3 +1,11 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const server = http.createServer((req, res) => {
+  res.end('Hello From Server!!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Start listenining to coming requests');
+});
